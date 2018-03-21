12fa5200af0281e6d9f464c11391350ab3f280a4
Jonathan Poncy <jonathan.poncy@tactfactory.com>
2018-03-21 14:39:45 +0100

    Use container trait

--- src/AppBundle/Controller/HelloController.php
+++ src/AppBundle/Controller/HelloController.php
@@ -2,15 +2,14 @@
 
 namespace AppBundle\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+use Symfony\Component\HttpFoundation\Response;
 
 class HelloController implements ContainerAwareInterface
 {
-
-    private $container;
+    /* Get container. */
+    use ContainerAwareTrait;
 
     /**
      * @Route(path = "/hello", name = "coucou")
@@ -19,17 +18,10 @@ class HelloController implements ContainerAwareInterface
      */
     public function helloAction()
     {
-        $name = $_POST['name'] ?? 'tout le monde';
-
         $twig = $this->container->get('twig');
         $content = $twig->render('Hello/hello.html.twig');
 
         $result = new Response($content, Response::HTTP_OK);
         return $result;
-    }
-
-    public function setContainer(ContainerInterface $container = null)
-    {
-        $this->container = $container;
     }
 }
